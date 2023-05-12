a83a4682a16815f46446ef314a03782ddff39345
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-05-12 20:42:49 -0300

    process

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const percentage = 43.67
+const process = document.querySelector(".process-bar-loading")
+process.setAttribute("style","width: "+percentage+"%")
+process.setAttribute("title",percentage+"%")

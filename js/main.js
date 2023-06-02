3aef511164fbd804a9940a8a36c9487c4796fc70
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-06-02 16:12:32 -0300

    manutenção

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const percentage = 43.67
+const percentage = 44.55
 const process = document.querySelector(".process-bar-loading")
 process.setAttribute("style","width: "+percentage+"%")
 process.setAttribute("title",percentage+"%")
